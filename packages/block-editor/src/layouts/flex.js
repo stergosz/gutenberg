ddd809959a7fcabcae4ed27764016c5c66d1014a
Riad Benguella <benguella@gmail.com>
2021-12-14 11:23:49 +0100

    Add correct style generation to the frontend hook

--- packages/block-editor/src/layouts/flex.js
+++ packages/block-editor/src/layouts/flex.js
@@ -84,13 +84,15 @@ export default {
 			</BlockControls>
 		);
 	},
-	save: function FlexLayoutStyle( { selector, layout } ) {
+	save: function FlexLayoutStyle( { selector, layout, style } ) {
 		const {
 			orientation = 'horizontal',
 			setCascadingProperties = false,
 		} = layout;
 		const blockGapSupport = useSetting( 'spacing.blockGap' );
 		const hasBlockGapStylesSupport = blockGapSupport !== null;
+		const blockGapValue =
+			style?.spacing?.blockGap ?? 'var( --wp--style--block-gap, 0.5em )';
 		const justifyContent =
 			justifyContentMap[ layout.justifyContent ] ||
 			justifyContentMap.left;
@@ -127,15 +129,12 @@ export default {
 			--layout-align: ${ alignItems };
 			`;
 		}
+
 		return (
 			<style>{ `
 				${ appendSelectors( selector ) } {
 					display: flex;
-					gap: ${
-						hasBlockGapStylesSupport
-							? 'var( --wp--style--block-gap, 0.5em )'
-							: '0.5em'
-					};
+					gap: ${ hasBlockGapStylesSupport ? blockGapValue : '0.5em' };
 					flex-wrap: ${ flexWrap };
 					${ orientation === 'horizontal' ? rowOrientation : columnOrientation }
 				}
